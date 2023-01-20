56f783cb4d16b6874e2bc941324d61e4d6fdb367
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-20 09:19:19 -0300

    menu open / close js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const navbar = document.getElementById('nav__items')
+const openBtm = document.querySelector('#open__nav-btn')
+const closeBtn = document.querySelector('#close__nav-btn')
+
+openBtm.addEventListener('click', () => {
+    navbar.style.display = 'flex'
+    openBtm.style.display = 'none'
+    closeBtn.style.display = 'inline-block'
+})
+
+closeBtn.addEventListener('click', () => {
+    navbar.style.display = 'none'
+    closeBtn.style.display = 'none'
+    openBtm.style.display = 'inline-block'
+})
